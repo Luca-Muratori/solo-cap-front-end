0952bdb13a8602e4cecfc075a6c6468bee1178f0
Lucai994 <luca.muratori.94@hotmail.it>
2022-06-29 07:49:14 +0200

    commit

--- src/components/WelcomePage/GoggleLogin.jsx
+++ src/components/WelcomePage/GoggleLogin.jsx
@@ -1,35 +1,43 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const GoggleLogin = () => {
   const navigate = useNavigate();
+  const params = useParams();
 
   const handleGoogleFailure = (response) => {
     alert(response);
   };
   const handleGoogleLogin = async () => {
-    console.log("hello from google");
-    const response = await fetch(
-      "https://solo-capstone.herokuapp.com/user/googleLogin ",
-      {
-        method: "GET",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.log(response);
-    if (response.status === 200) {
-      const data = await response.json();
-      navigate("/home");
-      console.log(data);
-      localStorage.setItem("token", data.accessToken);
-    } else {
-      alert("Invalid credentials");
-    }
+    console.log("params", params);
+    // const response = await fetch(
+    //   "https://solo-capstone.herokuapp.com/user/googleLogin ",
+    //   {
+    //     method: "GET",
+    //     headers: {
+    //       "Access-Control-Allow-Origin": "*",
+    //       "Content-Type": "application/json",
+    //     },
+    //   }
+    // );
+    // console.log(response);
+    // if (response.status === 200) {
+    //   const data = await response.json();
+    //   navigate("/home");
+    //   console.log(data);
+    //   localStorage.setItem("token", data.accessToken);
+    // } else {
+    //   alert("Invalid credentials");
+    // }
   };
-  return <a href={`${process.env.REACT_APP_URL}/user/googleLogin`}> Google</a>;
+  return (
+    <a
+      onClick={handleGoogleLogin}
+      href={`${process.env.REACT_APP_URL}/user/googleLogin`}
+    >
+      Google
+    </a>
+  );
 };
 
 export default GoggleLogin;
